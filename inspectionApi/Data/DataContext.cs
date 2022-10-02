5223107ee85432c0a22443ed0635ebb6acb6cade
yxtao <57578694+yxtao@users.noreply.github.com>
2022-10-02 14:54:32 -0700

    first commit

---
+++ inspectionApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using inspectionApi;
+
+namespace inspectionApi.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+        public DbSet<inspectionApi.InspectionType> InspectionType { get; set; }
+    }
+}
---
+++ inspectionApi/Status.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace inspectionApi
+{
+    public class Status
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string SatusOption { get; set; } = string.Empty;
+    }
+}
